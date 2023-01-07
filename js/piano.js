8476b41a0a68513a3e88635f9d557b9562f39062
Akshit Koduru <koduruakshit2003@gmail.com>
2023-01-07 06:21:33 +0530

    first commit

--- js/piano.js
+++ js/piano.js
@@ -3,34 +3,34 @@ let audio;
 let i=0;
 let txt="";
 const mus = {
-  A: new Audio("audio/Piano/A0.mp3"),
-  B: new Audio("audio/Piano/A1.mp3"),
-  C: new Audio("audio/Piano/A2.mp3"),
-  D: new Audio("audio/Piano/A3.mp3"),
-  E: new Audio("audio/Piano/A4.mp3"),
-  F: new Audio("audio/Piano/A5.mp3"),
-  G: new Audio("audio/Piano/A6.mp3"),
-  H: new Audio("audio/Piano/A7.mp3"),
-  I: new Audio("audio/Piano/Ab1.mp3"),
-  J: new Audio("audio/Piano/Ab2.mp3"),
-  K: new Audio("audio/Piano/Ab3.mp3"),
-  L: new Audio("audio/Piano/Ab4.mp3"),
-  M: new Audio("audio/Piano/Ab5.mp3"),
-  N: new Audio("audio/Piano/Ab6.mp3"),
-  O: new Audio("audio/Piano/Ab7.mp3"),
-  P: new Audio("audio/Piano/B0.mp3"),
-  Q: new Audio("audio/Piano/B1.mp3"),
-  R: new Audio("audio/Piano/B2.mp3"),
-  S: new Audio("audio/Piano/B3.mp3"),
-  T: new Audio("audio/Piano/B4.mp3"),
-  U: new Audio("audio/Piano/B5.mp3"),
-  V: new Audio("audio/Piano/B6.mp3"),  /*&#119187*/ 
-  W: new Audio("audio/Piano/B7.mp3"),
-  X: new Audio("audio/Piano/Bb1.mp3"),
-  Y: new Audio("audio/Piano/Bb2.mp3"), 
-  Z: new Audio("audio/Piano/Bb3.mp3"),
+  A: new Audio("audio/Piano/Bb3.mp3"),
+  B: new Audio("audio/Piano/B4.mp3"),
+  C: new Audio("audio/Piano/Bb1.mp3"),
+  D: new Audio("audio/Piano/B0.mp3"),
+  E: new Audio("audio/Piano/B7.mp3"),
+  F: new Audio("audio/Piano/A7.mp3"),
+  G: new Audio("audio/Piano/Ab3.mp3"),
+  H: new Audio("audio/Piano/B5.mp3"),
+  I: new Audio("audio/Piano/Bb0.mp3"),
+  J: new Audio("audio/Piano/B1.mp3"),
+  K: new Audio("audio/Piano/A6.mp3"),
+  L: new Audio("audio/Piano/Ab2.mp3"),
+  M: new Audio("audio/Piano/Bb2.mp3"),
+  N: new Audio("audio/Piano/A0.mp3"),
+  O: new Audio("audio/Piano/A5.mp3"),
+  P: new Audio("audio/Piano/Ab4.mp3"),
+  Q: new Audio("audio/Piano/Bb4.mp3"),
+  R: new Audio("audio/Piano/Ab1.mp3"),
+  S: new Audio("audio/Piano/A2.mp3"),
+  T: new Audio("audio/Piano/A4.mp3"),
+  U: new Audio("audio/Piano/Ab5.mp3"),
+  V: new Audio("audio/Piano/Ab7.mp3"),  /*&#119187*/ 
+  W: new Audio("audio/Piano/A3.mp3"),
+  X: new Audio("audio/Piano/A8.mp3"),
+  Y: new Audio("audio/Piano/Ab6.mp3"), 
+  Z: new Audio("audio/Piano/A1.mp3"),
  " ":new Audio("audio/Piano/silent_quarter-second.mp3"),  //&#119047;	\x1d107
- "#":new Audio("audio/Piano/Bb5.mp3")
+ "#":new Audio("audio/Piano/A1.mp3")
 };
 
 //onst startButton = document.getElementById('#startButton');//instead of using doublequotes
